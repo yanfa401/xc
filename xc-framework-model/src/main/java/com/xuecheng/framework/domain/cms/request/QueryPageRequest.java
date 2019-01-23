cab46185545a1532879bf45f0d254a7793057d65
Administrator <Administrator@Hasee-PC>
2019-01-23 23:01:19 +0800

    添加了swagger接口文档生成的注解

--- xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
+++ xc-framework-model/src/main/java/com/xuecheng/framework/domain/cms/request/QueryPageRequest.java
@@ -2,6 +2,7 @@ package com.xuecheng.framework.domain.cms.request;
 
 import com.xuecheng.framework.model.request.RequestData;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 /**
@@ -14,13 +15,18 @@ import lombok.Data;
 public class QueryPageRequest extends RequestData{
 
 	//站点id
+	@ApiModelProperty("站点id")
 	private String siteId;
 	//页面id
+	@ApiModelProperty("页面id")
 	private String pageId;
 	//页面名称
+	@ApiModelProperty("页面名称")
 	private String pageName;
 	//别名
+	@ApiModelProperty("别名")
 	private String pageAlias;
 	//模板id
+	@ApiModelProperty("模板id")
 	private String templateId;
 }
